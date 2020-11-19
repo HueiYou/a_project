63c734d01291ec671d2f12b039c6f9600735295d
Amy <z851772198@gmail.com>
2020-11-19 22:29:21 +0800

    test vue project

---
+++ test-project/src/App.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div>
+    <h1>{{name}}</h1>
+    <input type="text" v-model="myText" />
+    <button type="button" @click="add()">Add</button>
+    <h3>Amy Test babel</h3>
+    <Menu :myList="myList" :key="myList.length"></Menu>
+  </div>
+</template>
+
+<script>
+  import Vue from "vue";
+  import Menu from "./Menu";
+
+  export default Vue.extend({
+    data: function() {
+      return {
+        name: 'Hello World!',
+        myText: '2',
+        myList: [1,2,3]
+      }
+    },
+    methods: {
+      add () {
+        if (this.myText) {
+          this.myList.push(this.myText);
+          // this.myText = '';
+        }
+      }
+    },
+    components: {
+      Menu
+    }
+  });
+</script>
+
+
---
+++ test-project/src/Menu.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <ul>
+    <li v-for="(item, index) in myList" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<script>
+    export default {
+        props: ["myList"],
+    }
+</script>
